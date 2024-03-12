1595d0ddb737954c2302aaf8f515c1522b3eb15b
Rojo Valisoa ANDRIANANTENAINA <v.andrianantenaina@etech.local>
2024-03-12 22:13:16 +0300

    mat_calendar

---
+++ mat_calendar/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Iteam-$ CO
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'MATERAUTO Calendar',
+    'description': """
+            Materauto Calendar
+        """,
+    'version': '16.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'Rojodev ',
+    'website': 'https://iteam-s.mg/',
+    'depends': [
+        'base', 'contacts',
+    ],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
